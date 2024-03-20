57733630ffb42642b89fcba00f8ac74495554cf1
yocong <rnfaos77@naver.com>
2024-03-20 09:23:46 +0900

    # 조건연산자

---
+++ 1. core javascript/1. 자바스크립트 기초/6. 조건연산자.js
@@ -0,0 +1,9 @@
+var money = 5000;
+
+var food = money >= 8000 ? "돈까스" : "라면";
+
+console.log(`선택한 음식: ${food}`);
+
+var food = money >= 8000 ? "돈까스" : money >= 5000 ? "쫄면" : "라면";
+
+console.log(`선택한 음식: ${food}`);
