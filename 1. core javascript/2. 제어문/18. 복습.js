3a2e4b3ab8025a506a88d1c9022aba232cc8e92d
yocong <rnfaos77@naver.com>
2024-03-23 16:55:33 +0900

    # 사칙연산 2번

--- 1. core javascript/2. 제어문/18. 복습.js
+++ 1. core javascript/2. 제어문/18. 복습.js
@@ -1,4 +1,4 @@
-// 사칙연산 1번
+// 사칙연산 2번
 
 alert("~~~~~~~~~ 재미있는 사칙연산 게임 ~~~~~~~~");
 alert("즐겁게 문제를 푸시다가 지겨우면 0을 누르세요~");
@@ -6,6 +6,10 @@ alert("=========================================");
 
 // 카운트
 var count = 1;
+
+// 정답, 틀린횟수 카운트
+var correctCount = 0;
+var wrongCount = 0;
 
 while (true) {
   // 숫자 생성
@@ -15,16 +19,21 @@ while (true) {
   // 사용자 입력값
   var answer = +prompt(`Q${count}. ${num1} + ${num2} = ??`);
   count++;
+
   // 정답 판정
   if (answer === num1 + num2) {
     alert(`정답입니다.`);
+    correctCount++;
   } else if (answer !== num1 + num2 && answer !== 0) {
     alert(`틀렸어요 ~`);
+    wrongCount++;
   } else if (answer === 0) {
     alert(`게임을 종료합니다!`);
     break;
   }
 }
+alert(`정답횟수: ${correctCount}회, 틀린횟수: ${wrongCount}회`);
+
 //   // 카운트 판정
 //   if (countDown <= 0) {
 //     alert(`기회가 ${countDown}번 남았습니다.`);
