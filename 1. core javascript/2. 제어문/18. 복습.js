fa50a16d2c2c5c05937cf137cdd26da09f73f2de
yocong <rnfaos77@naver.com>
2024-03-23 16:54:16 +0900

    # 사칙연산 1번

--- 1. core javascript/2. 제어문/18. 복습.js
+++ 1. core javascript/2. 제어문/18. 복습.js
@@ -1,58 +1,35 @@
-// up-down 3번
+// 사칙연산 1번
 
-alert("~~~~~~~~~ 재미있는 UP&DOWN 게임 ~~~~~~~~");
+alert("~~~~~~~~~ 재미있는 사칙연산 게임 ~~~~~~~~");
+alert("즐겁게 문제를 푸시다가 지겨우면 0을 누르세요~");
+alert("=========================================");
 
-
-// 난이도 상수
-const HIGH = 1,
-  MIDDLE = 2,
-  LOW = 3;
-
-// 난이도 설정
-alert("[ 난이도를 설정하세요 ~~~! ] ");
-while (true) {
-  var level = +prompt("[ 1. 상 (기회 3번) | 2. 중 (기회 6번) | 3. 하 (기회 10번)]");
-  if (level === HIGH) {
-    var count = 3;
-  } else if (level === MIDDLE) {
-    var count = 6;
-  } else if (level === LOW) {
-    var count = 10;
-  } else {
-    +prompt("난이도를 숫자로 다시 입력해주세요!");
-    continue;
-  }
-  break;
-}
-
-// 정답 생성 (1 ~ 50)
-var secret = Math.floor(Math.random() * 50) + 1;
-
-// 입력기회 횟수 - 난이도에 따른 기회 횟수 대입
-var countDown = count;
+// 카운트
+var count = 1;
 
 while (true) {
+  // 숫자 생성
+  var num1 = Math.floor(Math.random() * 50) + 1;
+  var num2 = Math.floor(Math.random() * 50) + 1;
+
   // 사용자 입력값
-  var answer = +prompt(`[ 1 ~ 50 사이의 무작위 숫자를 ${countDown}번 안에 맞춰보세요!!! ]`);
-  countDown--;
-
+  var answer = +prompt(`Q${count}. ${num1} + ${num2} = ??`);
+  count++;
   // 정답 판정
-  if (answer === secret) {
-    alert(`딩동댕~~~~ !!!`);
+  if (answer === num1 + num2) {
+    alert(`정답입니다.`);
+  } else if (answer !== num1 + num2 && answer !== 0) {
+    alert(`틀렸어요 ~`);
+  } else if (answer === 0) {
+    alert(`게임을 종료합니다!`);
     break;
-  } else if (answer > secret) {
-    alert(`DOWN!!!!`);
-  } else {
-    alert(`UP!!!!`);
-  }
-  
-  // 카운트 판정
-  if (countDown <= 0) {
-    alert(`기회가 ${countDown}번 남았습니다.`);
-    alert(`기회를 모두 소진했습니다. GAME OVER!!`);
-    break;
-  } else {
-    alert(`기회가 ${countDown}번 남았습니다.`);
   }
 }
-
+//   // 카운트 판정
+//   if (countDown <= 0) {
+//     alert(`기회가 ${countDown}번 남았습니다.`);
+//     alert(`기회를 모두 소진했습니다. GAME OVER!!`);
+//     break;
+//   } else {
+//     alert(`기회가 ${countDown}번 남았습니다.`);
+//   }
