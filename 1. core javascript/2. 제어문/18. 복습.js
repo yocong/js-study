bb19bb1857854cabd3f33f8ac4656e31383064ba
yocong <rnfaos77@naver.com>
2024-03-23 17:16:23 +0900

    # 사칙연산 3번

--- 1. core javascript/2. 제어문/18. 복습.js
+++ 1. core javascript/2. 제어문/18. 복습.js
@@ -1,4 +1,4 @@
-// 사칙연산 2번
+// 사칙연산 3번
 
 alert("~~~~~~~~~ 재미있는 사칙연산 게임 ~~~~~~~~");
 alert("즐겁게 문제를 푸시다가 지겨우면 0을 누르세요~");
@@ -13,21 +13,46 @@ var wrongCount = 0;
 
 while (true) {
   // 숫자 생성
-  var num1 = Math.floor(Math.random() * 50) + 1;
-  var num2 = Math.floor(Math.random() * 50) + 1;
+  var num1 = Math.floor(Math.random() * 10) + 1;
+  var num2 = Math.floor(Math.random() * 10) + 1;
+
+  // 부호 생성
+  // 0, 1, 2 중에 하나가 생성되는 난수
+  var markNum = Math.floor(Math.random() * 3);
+
+  // 부호
+  var mark;
+
+  // 실제정답
+  var realAnswer;
+
+  switch (markNum) {
+    case 0:
+      mark = "+";
+      realAnswer = num1 + num2;
+      break;
+    case 1:
+      mark = "-";
+      realAnswer = num1 - num2;
+      break;
+    case 2:
+      mark = "*";
+      realAnswer = num1 * num2;
+      break;
+  }
 
   // 사용자 입력값
-  var answer = +prompt(`Q${count}. ${num1} + ${num2} = ??`);
+  var userAnswer = +prompt(`Q${count}. ${num1} ${mark} ${num2} = ??`);
   count++;
 
   // 정답 판정
-  if (answer === num1 + num2) {
+  if (userAnswer === realAnswer) {
     alert(`정답입니다.`);
     correctCount++;
-  } else if (answer !== num1 + num2 && answer !== 0) {
+  } else if (userAnswer !== realAnswer && userAnswer !== 0) {
     alert(`틀렸어요 ~`);
     wrongCount++;
-  } else if (answer === 0) {
+  } else if (userAnswer === 0) {
     alert(`게임을 종료합니다!`);
     break;
   }
