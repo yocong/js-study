7d5fca3e92c5b3e315999d36214daa823cedecbe
yocong <rnfaos77@naver.com>
2024-03-23 16:01:51 +0900

    # updown 2번

--- 1. core javascript/2. 제어문/18. 복습.js
+++ 1. core javascript/2. 제어문/18. 복습.js
@@ -1,14 +1,17 @@
-// up-down 1번
+// up-down 2번
 
 alert("~~~~~~~~~ 재미있는 UP&DOWN 게임 ~~~~~~~~");
-// 정답 생성 (1 ~ 100)
-var secret = Math.floor(Math.random() * 100) + 1;
+// 정답 생성 (1 ~ 50)
+var secret = Math.floor(Math.random() * 50) + 1;
 
+// 입력기회 횟수
+var count = 5;
 
 while (true) {
   // 사용자 입력값
-  var answer = +prompt("[ 1 ~ 100 사이의 무작위 숫자를 맞춰보세요!!!]");
-  
+  var answer = +prompt("[ 1 ~ 50 사이의 무작위 숫자를 5번 안에 맞춰보세요!!!]");
+  count--;
+
   // 정답 판정
   if (answer === secret) {
     alert(`딩동댕~~~~ !!!`);
@@ -17,6 +20,15 @@ while (true) {
     alert(`DOWN!!!!`);
   } else {
     alert(`UP!!!!`);
+  }
+  
+  // 카운트 판정
+  if (count <= 0) {
+    alert(`기회가 ${count}번 남았습니다.`);
+    alert(`기회를 모두 소진했습니다. GAME OVER!!`);
+    break;
+  } else {
+    alert(`기회가 ${count}번 남았습니다.`);
   }
 }
 
