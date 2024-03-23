4ce331de1d13373aeaacfb979d664ce48425dc45
yocong <rnfaos77@naver.com>
2024-03-23 17:37:26 +0900

    # 사칙연산 4번

--- 1. core javascript/2. 제어문/18. 복습.js
+++ 1. core javascript/2. 제어문/18. 복습.js
@@ -1,8 +1,25 @@
-// 사칙연산 3번
+// 사칙연산 4번
 
 alert("~~~~~~~~~ 재미있는 사칙연산 게임 ~~~~~~~~");
 alert("즐겁게 문제를 푸시다가 지겨우면 0을 누르세요~");
 alert("=========================================");
+
+while (true) {
+  // 난이도 설정
+  var level = +prompt("[ 1. 상 (1~100) | 2. 중 (1~50) | 3. 하 (1~20)]");
+
+  // 난이도에 따른 max 숫자 설정
+  var maxNumber;
+
+  if (level === 1) {
+    maxNumber = 100;
+  } else if (level === 2) {
+    maxNumber = 50;
+  } else if (level === 3) {
+    maxNumber = 20;
+  }
+  break;
+}
 
 // 카운트
 var count = 1;
@@ -13,8 +30,18 @@ var wrongCount = 0;
 
 while (true) {
   // 숫자 생성
-  var num1 = Math.floor(Math.random() * 10) + 1;
-  var num2 = Math.floor(Math.random() * 10) + 1;
+  var num1 = Math.floor(Math.random() * maxNumber) + 1;
+  var num2 = Math.floor(Math.random() * maxNumber) + 1;
+
+  // 앞의 숫자가 더 크게 설정
+  if (num1 < num2) {
+    var temp = num1;
+    num1 = num2;
+    num2 = temp;
+  } else if (num1 === num2) { // 같은 숫자 출제 X
+    continue;
+  }
+
 
   // 부호 생성
   // 0, 1, 2 중에 하나가 생성되는 난수
@@ -56,14 +83,16 @@ while (true) {
     alert(`게임을 종료합니다!`);
     break;
   }
+
+    // // 카운트 판정
+    // if (count <= 0) {
+    //   alert(`기회가 ${count}번 남았습니다.`);
+    //   alert(`기회를 모두 소진했습니다. GAME OVER!!`);
+    //   break;
+    // } else {
+    //   alert(`기회가 ${count}번 남았습니다.`);
+    // }
 }
 alert(`정답횟수: ${correctCount}회, 틀린횟수: ${wrongCount}회`);
 
-//   // 카운트 판정
-//   if (countDown <= 0) {
-//     alert(`기회가 ${countDown}번 남았습니다.`);
-//     alert(`기회를 모두 소진했습니다. GAME OVER!!`);
-//     break;
-//   } else {
-//     alert(`기회가 ${countDown}번 남았습니다.`);
-//   }
+
