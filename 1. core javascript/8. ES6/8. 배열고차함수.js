874dd3b785e49b2613eb9297a56dd4f8704755b0
yocong <rnfaos77@naver.com>
2024-03-28 21:55:27 +0900

    # filter, map

--- 1. core javascript/8. ES6/8. 배열고차함수.js
+++ 1. core javascript/8. ES6/8. 배열고차함수.js
@@ -69,3 +69,73 @@ console.log(`total: ${total}`);
 
 console.log('============================');
 
+// filter: 조건에 맞는 배열안의 요소를 필터링
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const filteredArray = numbers.filter(n => n % 3 === 0);
+console.log(filteredArray);
+
+// 회원목록에서 이름이 3글자인 회원들을 필터링해줘
+const filteredUserList
+  = userList.filter(user => user.userName.length === 8);
+console.log(filteredUserList);
+
+
+console.log('==========================');
+
+// map: 배열에서 특정 데이터들을 추출해서 새로운배열에 매핑
+
+// filter와 map의 공통점
+// b 부분에는 인덱스가 출력
+// traders
+//   .map((a, b) => {
+//     console.log(b);
+//   })
+
+// filter와 map의 차이점
+// filter => 10개의 데이터를 필터링하면 10개 이하로 남음
+//           ex) 10명 중 서울사는애들 필터링! -> 3명만 남음
+// map    => 10개의 데이터를 매핑하면 항상 10개짜리 배열이 나옴
+//           ex) 10명의 회원정보 중 이름정보만 10개 가져와
+
+const mappedNumbers = numbers.map(n => `<li class="list-item">${n}</li>`);
+console.log(mappedNumbers);
+
+// 회원목록에서 회원의 나이를 추출해서 xx세로 매핑
+const mappedUsers = userList.map(user => `${user.age}세`);
+console.log(mappedUsers);
+
+// 회원목록에서 회원 이름과 회원의 나이와 회원의 주소를 추출할건데
+// 이름의 키값을 nicknam, 나이는 old, 주소는 addr로 하고싶음
+// -> 3가지를 묶어야하므로 객체 사용!
+
+// 객체를 사용할 때 화살표함수를 사용한다면 {}로 묶는 것을 잊어서는 안됨
+// 그리고 꼭 중괄호를 return이나 ()로 묶어야한다!
+const mappedUserList = userList.map(user => {
+  return {
+  nickname: user.userName,
+  old: user.age,
+  addr: user.adde
+  }
+});
+
+// const mappedUserList = userList.map(function(user) { 
+//   return {
+//   nickname: user.userName,
+//   old: user.age,
+//   addr: user.address
+// }
+// });
+
+console.log(mappedUserList);
+
+
+console.log("=====================");
+
+// 처음 발견된
+const triple = numbers.find(n => n % 3 === 0);
+console.log(`triple: ${triple}`);
+
+const fifty = userList.some(user => user.age >= 50 && user.age < 60);
+console.log(fifty);
+
