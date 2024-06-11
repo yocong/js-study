a3437d0767df63d2f4d9358cca73912f15a82c81
yocong <rnfaos77@naver.com>
2024-06-11 11:02:26 +0900

    # 클래스를 따로 만들어 import하여 사용 -> 단순하게 사용가능

---
+++ 4. OOP/js/components/Product.js
@@ -0,0 +1,12 @@
+
+class Product {
+  constructor(title, price, desc, image) {
+    this.title = title;
+    this.price = price;
+    this.desc = desc;
+    this.image = image;
+  }
+}
+
+// export default : 한 모듈에 하나, {} 생략가능, 이름 변경도 가능
+export default Product;
